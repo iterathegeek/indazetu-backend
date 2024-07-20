ca894565f849f0a4938907a0616fddd683294c80
Adrian <iterathegeek@gmail.com>
2024-07-20 23:29:05 +0300

    initial commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: ['http://localhost:3000','https://indazetu-client.onrender.com'],
+  origin: ['http://localhost:3000','https://indazetu-client.onrender.com','https://indazetu.com'],
   credentials: true
 }));
 
