6e3579d3bfcd4aec74423e5320f691cbb92ca3eb
Adrian <iterathegeek@gmail.com>
2024-08-24 14:04:00 +0300

    email smtp

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -45,10 +45,10 @@ transporter.use('compile', hbs(handlebarOptions));
 const generateMessageEmailTemplate = async ({ subject, message }) => {
   return `
 
-    <div class="container">
+    <div>
       <div style="text-align:center">
         <img src="https://img.icons8.com/ios-filled/50/000000/key.png" alt="Activation Icon">
-        <h2 style="margin-left:20%">${subject}</h2>
+        <h2 style="margin-left:5%">${subject}</h2>
       </div>
       
       <div class="message">
